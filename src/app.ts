3a3588b16736a6fcc83f940911152c8d7776ddd8
silviog1990 <gaysilvio90@gmail.com>
2020-05-09 12:33:44 +0200

    Add singleton for nodejs events and handle graceful shutdown

--- src/app.ts
+++ src/app.ts
@@ -8,6 +8,9 @@ import directorsRouter from './routes/directors';
 import moviesRouter from './routes/movies';
 import { DBConnection } from './db';
 import { CacheConnection } from './cache';
+import { EventEmitter } from 'events';
+import logger from './utils/logger';
+import { Events } from './utils/events';
 
 // init express
 const app = express();
@@ -27,8 +30,52 @@ if (NODE_ENV === ENVIRONMENT.PRODUCTION) {
 const db = DBConnection.getInstance();
 const cache = CacheConnection.getInstance();
 
+app.get('/health', (req, res, next) => {
+    res.send();
+});
+
 app.use('/api/v1/auth', authRouter);
 app.use('/api/v1/directors', directorsRouter);
 app.use('/api/v1/movies', moviesRouter);
+
+const shutdown = () => {
+    db.disconnect();
+    cache.disconnect();
+    const disconnectedPromises = new Array<Promise<void>>();
+    const eventEmitter = Events.getInstance().getEventEmitter();
+
+    disconnectedPromises.push(
+        new Promise((resolve, reject) => {
+            eventEmitter.on('dbDisconnected', () => {
+                logger.debug('event db disconnected');
+                resolve();
+            });
+        })
+    );
+
+    disconnectedPromises.push(
+        new Promise((resolve, reject) => {
+            eventEmitter.on('cacheDisconnected', () => {
+                logger.debug('event cache disconnected');
+                resolve();
+            });
+        })
+    );
+
+    // forced shutdown
+    setTimeout(() => {
+        console.error('Could not close connections in time, forcefully shutting down');
+        process.exit(1);
+    }, 10000);
+
+    // graceful shutdown
+    Promise.all(disconnectedPromises).then(() => {
+        logger.info('Shutdown completed.');
+        process.exit(0);
+    });
+};
+
+process.on('SIGTERM', shutdown);
+process.on('SIGINT', shutdown);
 
 export default app;
